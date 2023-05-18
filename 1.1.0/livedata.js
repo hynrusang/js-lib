cf16322497fa6992ecf1e60185dc7300e23b2c31
hynrusang <mashmalo1871@gmail.com>
2023-05-19 03:59:17 +0900

    up

---
+++ 1.1.0/livedata.js
@@ -0,0 +1,44 @@
+﻿/*
+이 소스코드는 js로 kotlin의 LiveData를 구현한 소스코드입니다.
+절대로 무단으로 가져가지 마시기 바랍니다.
+작성자: 환류상
+ */
+const LiveData = class {
+    #data;
+    #observer;
+    /**
+     * @type {(data: Any) => LiveData}
+     */
+    set value(data) {
+        const isChanged = (JSON.stringify(data) != JSON.stringify(this.#data)) ? true : false;
+        this.#data = data;
+        if (isChanged && typeof this.#observer == "function") this.#observer();
+        return this;
+    }
+    /**
+     * @type {() => Any}
+     */
+    get value() {
+        return (Array.isArray(this.#data)) ? [...this.#data] : (typeof this.#data == "object") ? Object.assign({}, this.#data) : this.#data;
+    }
+    /**
+     * @type {(observer: Function) => LiveData}
+     */
+    registObserver = observer => {
+        this.#observer = observer;
+        return this;
+    }
+    constructor(data) {
+        this.#data = data;
+    }
+}
+JSON.unlivedata = json => {
+    let data = {};
+    for (let key of Object.keys(json)) data[key] = (json[key] instanceof LiveData) ? json[key].get() : json[key];
+    return data;
+}
+Array.unlivedata = array => {
+    let data = []
+    for (let value of array) data.push((value instanceof LiveData) ? value.get() : value);
+    return data;
+}
