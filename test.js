3d0c049e2dc3497ad6d3fa916f46caefb3541111
hynrusang <mashmalo1871@gmail.com>
2023-05-19 16:07:09 +0900

    up

---
+++ test.js
@@ -0,0 +1,32 @@
+﻿const makeTestDom = (alphabet, fragmentIdentity) => $("fieldset").add(
+    $("legend", {text: `${fragmentIdentity} fragment`, style: "color: red;"}),
+    $("input", {type: "button", value: alphabet, style: "height: 200px; width: 200px; margin-left: calc(50% - 100px); font-size: 150px;", onclick: () => {
+        alert(`you clicked ${alphabet}!`);
+    }})
+)
+
+const state = new LiveData("first").setObserver(function () {
+    console.log(`${this.get()} Fragment is stating now...`);
+    scan("nav").innerHTML = `Current-Fragment://${this.value}`
+});
+
+const fragmentBox = [new Fragment("fragmentView", makeTestDom("A", "first")).registAnimation(FragAnimation.card, 1).registAction(() => {
+    state.value = "first";
+}).launch(),
+new Fragment("fragmentView", makeTestDom("B", "second")).registAnimation(FragAnimation.fade, 1).registAction(() => {
+    state.value = "second";
+}),
+new Fragment("fragmentView", makeTestDom("C", "third")).registAnimation(FragAnimation.swip, 1).registAction(() => {
+    state.value = "third";
+}),
+new Fragment("fragmentView", makeTestDom("D", "fourth")).registAction(() => {
+    state.value = "fourth";
+})];
+
+scan("!footer input").forEach((element, index) => {
+    element.onclick = () => {
+        scan(".activate").classList.remove("activate");
+        element.classList.add("activate");
+        fragmentBox[index].launch();
+    }
+});
