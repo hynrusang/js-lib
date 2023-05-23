6e1a90a59676295a8d1938991ce4f391f1bc3d6c
hynrusang <mashmalo1871@gmail.com>
2023-05-23 16:44:35 +0900

    Update advanced.js

--- 1.0.0/advanced.js
+++ 1.0.0/advanced.js
@@ -18,8 +18,16 @@ const wait = millisecond => new Promise(code => setTimeout(code, millisecond));
 * }}
 */
 const getIndex = (parent, child) => Array.prototype.indexOf.call((parent.nodeName != null) ? parent.children : parent, child);
-String.prototype.isEmpty = function () { return (this.length == 0); }
-Array.prototype.isEmpty = function () { return (this.length == 0); }
+String.prototype.isEmpty = function (...ignore) { 
+    let __$$TEMPSTRING = this;
+    for (let __$$IGNORE of ignore) __$$TEMPSTRING = __$$TEMPSTRING.replaceAll(__$$IGNORE, "");
+    return __$$TEMPSTRING.length === 0;
+}
+Array.prototype.isEmpty = function (...ignore) { 
+    let __$$TEMPARRAY = [];
+    for (let __$$DATA of this) if (!ignore.includes(__$$DATA)) __$$TEMPARRAY.push(__$$DATA);
+    return __$$TEMPARRAY.length === 0; 
+}
 Array.prototype.add = function (data) { 
     this.push(data);
     return this;
