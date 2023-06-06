0346a97c0d4d672dc334c63ca30c8e9869c54d42
hynrusang <mashmalo1871@gmail.com>
2023-06-06 15:47:46 +0900

    prerelease
    livedata 1.2.0 모델링 시작

---
+++ 1.2.0/livedata.js
@@ -0,0 +1,66 @@
+﻿/*
+이 소스코드는 js로 kotlin의 LiveData를 구현한 소스코드입니다.
+작성자: 환류상
+ */
+const LiveData = class {
+    #data;
+    #observer;
+    #allowed;
+    set value(data) {
+        if (this.#allowed && this.#allowed.name.toLocaleLowerCase() !== (Array.isArray(data) ? "array" : typeof data)) throw new TypeError(`invalid type of data. Data must be of type ${this.#allowed.name}.`);
+        const isChanged = (JSON.stringify(data) != JSON.stringify(this.#data)) ? true : false;
+        this.#data = data;
+        if (isChanged && typeof this.#observer == "function") this.#observer();
+    }
+    get value() {
+        return (Array.isArray(this.#data)) ? [...this.#data] : (typeof this.#data == "object") ? Object.assign({}, this.#data) : this.#data;
+    }
+    /**
+     * @type {(observer: Function) => LiveData}
+     */
+    registObserver = observer => {
+        this.#observer = observer;
+        return this;
+    }
+    /**
+     * @deprecated This can cause unintended behavior.
+     * @type {() => void}
+     */
+    dispatchObserver = () => this.#observer();
+    /**
+     * @type {(data: Any, allowed: Type) => LiveData}
+     */
+    constructor(data, allowed) {
+        this.#data = data;
+        this.#allowed = allowed;
+    }
+}
+const LiveDataManager = class {
+    #editable;
+    #resource;
+    value = (id, data) => (typeof data !== "undefined") ? this.#resource[id].value = data : this.#resource[id].value;
+    get id() {
+        if (!this.#editable) throw new SyntaxError(`This LiveDataManager cannot be accessed or modified externally.`)
+        return this.#resource;
+    }
+    toArray = () => Object.values(this.#resource).map(inner => inner.value);
+    toObject = () => Object.entries(this.#resource).reduce((obj, [key, value]) => {
+        obj[key] = value.value;
+        return obj;
+      }, {});
+    /**
+     * @type {(livedataObject: Object, editable?: boolean) => LiveDataManager}
+     */
+    constructor(livedataObject, editable = true) {
+        if ("object" !== (Array.isArray(livedataObject) ? "array" : typeof livedataObject)) throw new TypeError(`invalid type of livedataObject. livedataObject must be of type Object. (livedataObject: ${Array.isArray(livedataObject) ? "array" : typeof livedataObject})`);
+        this.#editable = editable;
+        this.#resource = Object.entries(livedataObject).reduce((obj, [key, value]) => { 
+            if (!(value instanceof LiveData)) throw new TypeError(`invalid type of ${key}'s value. ${key}'s value must be of instance LiveData`)
+            else obj[key] = value;
+            return obj;
+        }, {});
+    }
+}
+const Binder = class {
+    
+}
