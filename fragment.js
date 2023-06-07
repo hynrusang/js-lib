268dc2cc1e7789faaba5360dd2bacece2836e9a6
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-06-07 17:55:21 +0900

    Update fragment.js

--- fragment.js
+++ fragment.js
@@ -41,10 +41,10 @@ const fragmentList = {
         ),
         $("fieldset").add(
             $("legend", {text: "html binding example"}),
-            $("input", {value: "hello, world!", style: "width: 100%; height: 200px;", var: "greeting"}),
-            $("input", {type: "text", value: "6", style: "width: 50%; height: 40px; text-align: center;", var: "a"}),
-            $("input", {type: "text", value: "11", style: "width: 50%; height: 40px; text-align: center;", var: "b"}),
-            $("p", {exp: "greeting a b->{greeting} {a} + {b} = {a + b} (각 값들은 숫자가 아닌 문자열도 지원합니다.)"}),
+            $("input", {value: "binding example", style: "width: 100%; height: 200px;", var: "a"}),
+            $("input", {type: "text", value: "6", style: "width: 50%; height: 40px; text-align: center;", var: "b"}),
+            $("input", {type: "text", value: "11", style: "width: 50%; height: 40px; text-align: center;", var: "c"}),
+            $("p", {exp: "a b c->{a}: {b} + {c} = {b + c} (각 값들은 숫자가 아닌 문자열도 지원합니다.)"}),
             $("input", {type: "button", value: "binding된 요소 초기화", style: "width: 300px; height: 40px; margin-left: 15px; margin-bottom: 10px;", onclick: e => {
                 scan("!fieldset input")[0].value = "hello, world!";
                 scan("!fieldset input")[1].value = 6;
